5b8c62c5f582a075b334e06667b98e5721063d2d
Aditya Verma <ronnaditya@gmail.com>
2026-05-18 15:38:35 +0530

    Revert "p-10 v1.0"
    This reverts commit c1154d07e6098787fd73930c746517325840e4cf.

--- p-10/p-10.c
+++ p-10/p-10.c
@@ -1,28 +1,3 @@
-#include<stdio.h>
-
 bool isValid(char* s) {
-  // What's new:
-  // - Unlike python, cannot simply iterate over object when the size of array is not available
-  char* open_arr[] = {"(", "[", "{"};
-  char* closed_arr[] = {")", "]", "}"};
   
-  char* curr_arr[] = {};
-  int curr_count = 0;
-  int index;
-  int i = 0;
-  while (s[i] != 0) {
-    for (int j = 0; j < 3; ++j) {
-      if (s[i] == open_arr[j]) {
-        index = j;
-        curr_arr[curr_count] = s[i];
-        curr_count += 1;
-      } else {
-      }
-    }
-  }
-}
-
-int main() {
-  isValid("asfddsf");
-  return 0;
 }
