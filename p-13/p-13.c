fcf793c253a0120be4928a2f164ed182d0a738b2
Aditya Verma <ronnaditya@gmail.com>
2026-05-19 16:57:23 +0530

    [ ERROR ] brewed a control flow soup

--- p-13/p-13.c
+++ p-13/p-13.c
@@ -1,49 +1,57 @@
 #include<stdio.h>
+#include<string.h>
 
 struct dict {
   char roman;
   int number;
 };
 
-int romanToInt(char* s) {
-  struct dict I;
-  struct dict V;
-  struct dict X;
-  struct dict L;
-  struct dict C;
-  struct dict D;
-  struct dict M;
+struct dict characters[] = {
+    { 'V', 5 },
+    { 'X', 10 },
+    { 'L', 50 },
+    { 'C', 100 },
+    { 'D', 500 },
+    { 'M', 1000 }
+};
 
-  struct dict characters[] = {
-    I,
-    V,
-    X,
-    L,
-    C,
-    D,
-    M
-  };
-
-  I.roman = 'I';
-  I.number = 1;
-  V.roman = 'V';
-  V.number = 5;
-  X.roman = 'X';
-  X.number = 10;
-  L.roman = 'L';
-  L.number = 50;
-  C.roman = 'C';
-  C.number = 100;
-  D.roman = 'D';
-  D.number = 500;
-  M.roman = 'M';
-  M.number = 1000;
-
+int translate(char roman) {
   for (int i = 0; i < 7; ++i) {
-    printf("%c\n", characters[i].roman);
+    if (roman == characters[i].roman) {
+      return characters[i].number;
+    }
   }
 }
 
+int romanToInt(char* s) {
+  int length = strlen(s);
+
+  char autonomous[] = {'X', 'C', 'V', 'L', 'D'};
+  char dependents[] = {'I', 'X', 'C', 'M'};
+  int autonomous_length = 5;
+  int dependents_length = 4;
+  
+  int number = 0;
+  int i = 0;
+  while (i < length) {
+    for (int j = 0; j < dependents_length; ++j) {
+      if (s[i] == dependents[j]) {
+        for (int k = 0; k < autonomous_length; ++k) {
+          if (s[i + 1] == autonomous[k]) {
+            number += translate(s[i + 1]) - translate(s[i]);
+            ++i;
+            break;
+          } else {
+            number += translate(s[i]);
+          }
+        }
+      }
+    }
+  ++i;
+  }
+  return number;
+}
+
 void main () {
-  romanToInt("dsaf");
+  printf("%d", romanToInt("XL"));
 }
