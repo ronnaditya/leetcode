ea7790c3ad4690d75d886e54a9d229444e63a381
Aditya Verma <ronnaditya@gmail.com>
2026-05-20 21:06:42 +0530

    [ ERROR ] incorrect algorithm

--- p-27/p-27.c
+++ p-27/p-27.c
@@ -4,11 +4,15 @@ int removeElement(int* nums, int numsSize, int val) {
   int i = 0;
   while (i < numsSize) {
     if (nums[i] == val) {
+      // The memory address after the last item of the current array is accessed in this algorithm.
+      // That's the reason for it not working, as the second to last item is replaced by the item
+      // after the last memory address of the array, and the last `val` is placed at the address
+      // after the array.
       int j = i + 1;
-      while (nums[j] == val) {
-        // stack overflow
+      while (nums[j] == val & j < numsSize) {
         ++j;
       }
+      printf("i: %d | j: %d | nums[i]: %d | nums[j]: %d\n", i, j, nums[i], nums[j]);
       nums[i] = nums[j];
       nums[j] = val;
     }
@@ -22,6 +26,6 @@ int removeElement(int* nums, int numsSize, int val) {
 
 
 void main() {
-  int nums[] = {1,2,3,2,4,3,2,2,5,7,8,9,0};
-  removeElement(nums, 13, 2);
+  int nums[] = {1,2,3,2,4,3,2};
+  removeElement(nums, 7, 3);
 }
