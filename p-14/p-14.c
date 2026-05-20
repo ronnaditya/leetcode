ed82398b9c8cd0c17b5ee9ce159dcbc08e8f2573
Aditya Verma <ronnaditya@gmail.com>
2026-05-20 14:24:07 +0530

    [ EDIT ] added a .gitignore file

--- p-14/p-14.c
+++ p-14/p-14.c
@@ -1,11 +1,11 @@
 #include<stdio.h>
 
 char* longestCommonPrefix(char** strs, int strsSize) {
-    
+
 }
 
 void main() {
   char* strings[] = {"flow", "flower", "flight"};
 
-  printf("%s", longestCommonPrefix(strings, 3)); // array-to-pointer decay
+  printf("%s", longestCommonPrefix(strings, 3));
 }
