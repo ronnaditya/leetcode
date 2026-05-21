9470451a97351ffcd25ece0bc8f60ffc8084519a
Aditya Verma <ronnaditya@gmail.com>
2026-05-21 20:28:08 +0530

    Claude gave me a solution. Claude's solution is founded on difference, whereas mine is founded on identity. I will still try to making mine work. Just need to figure out a mark of uniqueness (the k increment).

--- p-26/p-26.c
+++ p-26/p-26.c
@@ -1,10 +1,30 @@
 #include<stdio.h>
 
 int removeDuplicates(int* nums, int numsSize) {
-
-  for (int i = 0; i < numsSize; ++i) {
-    printf("%d\n", nums[i]);
+  int i = 0;
+  int k = 0;
+  while (i <= numsSize - k) {
+    int j = i + 1;
+    while (j < numsSize && nums[i] == nums[j]) {
+      ++j;
+    }
+    ++k;
+    int l = j - 1;
+    while (l > i) {
+      int m = l;
+      while (m < numsSize - 1) {
+        nums[m] = nums[m + 1];
+        ++m;
+      }
+      nums[numsSize - 1] = nums[i];
+      --l;
+    }
+    ++i;
   }
+  for (int i = 0; i < numsSize; ++i) {
+    printf("%d", nums[i]);
+  }
+  return k;
 }
 
 void main() {
@@ -12,6 +32,8 @@ void main() {
   // and return the number of elements in the set.
   // int nums[] = {1,1,2,3,3,3,3,4,5};
   // int nums[] = {1, 2, 3, 3, 4,4,5,5,5,6,6};
-  int nums[] = {1, 1, 2};
-  removeDuplicates(nums, 3);
+  // int nums[] = {1, 1, 2};
+  int nums[] = {0,0,1,1,1,2,2,3,3,4};
+  // int nums[] = {1,2};
+  printf("%d", removeDuplicates(nums, 10));
 }
