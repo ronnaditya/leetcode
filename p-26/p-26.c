4d192c7bb9b8b1419272f8dad1f6869bf545bfce
Aditya Verma <ronnaditya@gmail.com>
2026-05-21 17:54:56 +0530

    [ ERROR ] i thought it was solved, but the compiler with address sanitiser throws an error

--- p-26/p-26.c
+++ p-26/p-26.c
@@ -1,29 +1,34 @@
 #include<stdio.h>
 
 int removeDuplicates(int* nums, int numsSize) {
-  // Difference is the mark of uniquencess.
   int i = 0;
   while (i < numsSize) {
-    if (nums[i] == nums[i + 1]) {
-      int tmp = nums[i + 1];
-      for (int j = i + 1; j < numsSize - 1; ++j) {
-        printf("%d switched with %d\n", nums[j], nums[j + 1]);
-        nums[j] = nums[j + 1];
+    int j = i + 1;
+    while (nums[i] == nums[j]) {
+      ++j;
+    }
+    int l = j - 1;
+    while (l > i) {
+      int m = l;
+      while (m < numsSize) {
+        nums[m] = nums[m + 1];
+        ++m;
       }
-      nums[numsSize - 1] = tmp;
+      nums[numsSize - 1] = nums[i];
+      --l;
     }
     ++i;
   }
-
   for (int i = 0; i < numsSize; ++i) {
-    printf("%d", nums[i]);
+    printf("%d\n", nums[i]);
   }
-
 }
 
 void main() {
   // Make a set out of the passed list, albeit maintaining the length of the list itself,
   // and return the number of elements in the set.
-  int nums[] = {1,1,2,3,3,3,3,4,5};
-  removeDuplicates(nums, 9);
+  // int nums[] = {1,1,2,3,3,3,3,4,5};
+  // int nums[] = {1, 2, 3, 3, 4,4,5,5,5,6,6};
+  int nums[] = {1, 1, 2};
+  removeDuplicates(nums, 3);
 }
