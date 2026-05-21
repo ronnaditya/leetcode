96431caf0d63c7ff96372d9aa2038b28a258c143
Aditya Verma <ronnaditya@gmail.com>
2026-05-21 15:59:51 +0530

    kinda' works, but i have a better idea

--- p-26/p-26.c
+++ p-26/p-26.c
@@ -1,12 +1,29 @@
 #include<stdio.h>
 
 int removeDuplicates(int* nums, int numsSize) {
-  
+  // Difference is the mark of uniquencess.
+  int i = 0;
+  while (i < numsSize) {
+    if (nums[i] == nums[i + 1]) {
+      int tmp = nums[i + 1];
+      for (int j = i + 1; j < numsSize - 1; ++j) {
+        printf("%d switched with %d\n", nums[j], nums[j + 1]);
+        nums[j] = nums[j + 1];
+      }
+      nums[numsSize - 1] = tmp;
+    }
+    ++i;
+  }
+
+  for (int i = 0; i < numsSize; ++i) {
+    printf("%d", nums[i]);
+  }
+
 }
 
 void main() {
   // Make a set out of the passed list, albeit maintaining the length of the list itself,
   // and return the number of elements in the set.
-  int nums[] = {0,0,0,1,1,2,3,3,3,3,4,5};
-  removeDuplicates(nums, 12);
+  int nums[] = {1,1,2,3,3,3,3,4,5};
+  removeDuplicates(nums, 9);
 }
