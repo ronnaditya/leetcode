005dd4fae85d5d6ad68b0e7380a5d9a7f282df13
Aditya Verma <ronnaditya@gmail.com>
2026-05-22 17:31:12 +0530

    [ ORIGINAL PROBLEM ] instantiate a linked list representing an integer in reversed order

--- p-2/p-2.c
+++ p-2/p-2.c
@@ -1,4 +1,5 @@
 #include<stdio.h>
+#include<stdlib.h>
 
 struct ListNode {
   int val;
@@ -10,28 +11,24 @@ struct ListNode* addTwoNumbers(struct ListNode* l1, struct ListNode* l2) {
 }
 
 void main() {
-  struct ListNode l1;
-  struct ListNode l2;
+  int a[] = {3, 4, 2};
+  int b[] = {4, 6, 5};
 
-  l1.val = 3;
-  l2.val = 4;
-
-  int a[] = {4,2};
-  int b[] = {6,5};
-
-  for (int i = 0; i < 2; ++i) {
-    struct ListNode l1_tmp;
-    struct ListNode l2_tmp;
-
-    l1_tmp.val = a[i];
-    l1_tmp.next = &l1;
-    l2_tmp.val = b[i];
-    l2_tmp.next = &l2;
-
-    l1 = *l1_tmp.next;
-    l2 = *l2_tmp.next;
-
-  printf("%d", l1.next->val);
-  }
+  struct ListNode l11;
+  l11.val = 3;
+  l11.next = NULL;
+  struct ListNode *ptr_l11 = malloc(sizeof(l11));
+  ptr_l11 = &l11;
   
+  struct ListNode l12;
+  l12.val = 4;
+  l12.next = ptr_l11;
+  struct ListNode *ptr_l12 = malloc(sizeof(l12));
+  ptr_l12 = &l12;
+
+  struct ListNode l13;
+  l13.val = 2;
+  l13.next = ptr_l12;
+  struct ListNode *ptr_l13 = malloc(sizeof(l13));
+  ptr_l13 = &l13;
 }
---
+++ p-2/p-2_construct-linked-list.c
@@ -0,0 +1,7 @@
+// Original Problem: Based on the given struct expressing a node of a linked list, construct
+// a linked list representing a given integer in reversed order.
+
+struct ListNode {
+  int val;
+  struct ListNode *next;
+};
