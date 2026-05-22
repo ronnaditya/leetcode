b44792d542db4eff7b20554f0454a4ba9b766fe1
Aditya Verma <ronnaditya@gmail.com>
2026-05-22 17:13:03 +0530

    [ ERROR ] cannot construct a linked list

--- p-2/p-2.c
+++ p-2/p-2.c
@@ -1,12 +1,37 @@
+#include<stdio.h>
+
 struct ListNode {
   int val;
   struct ListNode *next;
-  };
+};
 
 struct ListNode* addTwoNumbers(struct ListNode* l1, struct ListNode* l2) {
     
 }
 
 void main() {
+  struct ListNode l1;
+  struct ListNode l2;
+
+  l1.val = 3;
+  l2.val = 4;
+
+  int a[] = {4,2};
+  int b[] = {6,5};
+
+  for (int i = 0; i < 2; ++i) {
+    struct ListNode l1_tmp;
+    struct ListNode l2_tmp;
+
+    l1_tmp.val = a[i];
+    l1_tmp.next = &l1;
+    l2_tmp.val = b[i];
+    l2_tmp.next = &l2;
+
+    l1 = *l1_tmp.next;
+    l2 = *l2_tmp.next;
+
+  printf("%d", l1.next->val);
+  }
   
 }
