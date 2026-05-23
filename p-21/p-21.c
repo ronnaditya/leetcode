728e014278700010e6da96ac35dcb36b45a7665d
Aditya Verma <ronnaditya@gmail.com>
2026-05-23 15:25:27 +0530

    [ SOLVED ] Problem 21\n The only thing I didn't get right was pointing 'tai' to the 'dummy'.

--- p-21/p-21.c
+++ p-21/p-21.c
@@ -1,35 +1,34 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     struct ListNode *next;
- * };
- */
 struct ListNode* mergeTwoLists(struct ListNode* list1, struct ListNode* list2) {
     struct ListNode dummy;
-    struct ListNode* tail;
-    dummy.next = tail;
+    struct ListNode* tail = &dummy;
+    dummy.next = NULL;
     
-    int smaller;
-    while (l1 != NULL || l2 != NULL) {
-        if (l1->val < l2->val) {
-            smaller = 0;
+    int nextNode;
+    while (list1 != NULL || list2 != NULL) {
+      if (list1 && list2) {
+        if (list1->val < list2->val) {
+          nextNode = 0;
         } else {
-            smaller = 1;
+          nextNode = 1;
         }
-        
-        struct ListNode* node = malloc(sizeof(struct ListNode));
-        if (!smaller) {
-          node->val = l1->val;
-          tail->next = node;
-          tail = node;
-          l1 = l1->next;
-        } else {
-          node->val = l2->val;
-          tail->next = node;
-          tail = node;
-          l2 = l2->next;
+      } else if (list1) {
+        nextNode = 0;
+      } else {
+        nextNode = 1;
+      }
 
-        }
+      struct ListNode* node = malloc(sizeof(struct ListNode));
+      if (!nextNode) {
+        node->val = list1->val;
+        node->next = NULL;
+        list1 = list1->next;
+      } else {
+        node->val = list2->val;
+        node->next = NULL;
+        list2 = list2->next;
+      }
+      tail->next = node;
+      tail = node;
     }
+  return dummy.next;
 }
