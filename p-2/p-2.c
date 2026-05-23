a819a2ce363426fe80f69a1bdb2670099f6940bb
Aditya Verma <ronnaditya@gmail.com>
2026-05-23 13:12:20 +0530

    [ PROGRESS ] blueprint

--- p-2/p-2.c
+++ p-2/p-2.c
@@ -1,3 +1,4 @@
+#include<math.h>
 #include<stdio.h>
 #include<stdlib.h>
 
@@ -7,7 +8,7 @@ struct ListNode {
 };
 
 struct ListNode* listConstructor(int num) {
-  int* numArray = malloc(sizeof(int) * 2);
+  int* numArray = malloc(sizeof(int) * 10000000);
 
   int count = 0;
   while (num != 0) {
@@ -16,20 +17,49 @@ struct ListNode* listConstructor(int num) {
     num /= 10;
     ++count;
   }
+  struct ListNode* ptr_l = malloc(sizeof(struct ListNode) * count);
+  struct ListNode* next = NULL;
+  for (int i = count - 1; i >= 0; --i) {
+    ptr_l[i].val = numArray[i];
+    ptr_l[i].next = next;
+    next = &ptr_l[i];
+  }
+  free(numArray);
+  return ptr_l;
 }
 
 struct ListNode* addTwoNumbers(struct ListNode* l1, struct ListNode* l2) {
+  int n1 = 0;
+  int n2 = 0;
+
+  int power1 = 0;
+  while (l1->next != NULL) {
+    n1 += l1->val * (pow(10, power1));
+    l1 = l1->next;
+    ++power1;
+  }
+  n1 += l1->val * pow(10, power1);
+  
+  int power2 = 0;
+  while (l2->next != NULL) {
+    n2 += l2->val * pow(10, power2);
+    l2 = l2->next;
+    power2 += 1;
+  }
+  n2 += l2->val * pow(10, power2);
+  
+  int n3 = n1 + n2;
+
+  return listConstructor(n3);
 }
 
 void main() {
-  struct ListNode* l = listConstructor(123125);
+  struct ListNode* l1 = listConstructor(342);
+  struct ListNode* l2 = listConstructor(465);
 
-  int i = 0;
-  struct ListNode current = *l;
-  while (i < 5) {
-    printf("%d", current.val);
-    current = *current.next;
-    ++i;
-  }
+  struct ListNode* l3 = addTwoNumbers(l1, l2);
 
+  free(l1);
+  free(l2);
+  free(l3);
 }
