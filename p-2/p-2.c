2c57dd1032238e7a00c1552de36d4d01530e6b18
Aditya Verma <ronnaditya@gmail.com>
2026-05-23 14:21:57 +0530

    [ SOLVED ] used help from Claude, but there are algorithms even more efficient than this

---
+++ p-2/p-2.c
@@ -0,0 +1,23 @@
+struct ListNode* addTwoNumbers(struct ListNode* l1, struct ListNode* l2) {
+  struct ListNode dummy;
+  struct ListNode* tail = &dummy;
+
+  dummy.next = tail;
+  
+  int carry = 0;
+  while(l1 != NULL || l2!= NULL || carry) {
+    int sum = carry;
+    if (l1) {sum += l1->val; l1 = l1->next};
+    if (l2) {sum += l2->val; l2 = l2->next};
+    
+    carry = sum / 10;
+
+    struct ListNode* node = malloc(sizeof(struct ListNode));
+    node->val = sum % 10;
+    node->next = NULL;
+    tail->next = node;
+    tail = node;
+
+  }
+  return dummy.next;
+}
