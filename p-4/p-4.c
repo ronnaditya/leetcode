4a6f90974197cfb2c53f8aeec6fdc2cde543fae1
Aditya Verma <ronnaditya@gmail.com>
2026-05-25 19:35:21 +0530

    [ PARTIAL ] Address Sanitation issues

--- p-4/p-4.c
+++ p-4/p-4.c
@@ -9,24 +9,34 @@ double findMedianSortedArrays(int* nums1, int nums1Size, int* nums2, int nums2Si
   int i = 0;
   int j = 0;
   while ((i < nums1Size || j < nums2Size) && (numsIndex < nums1Size + nums2Size)) {
-    if (nums1[i] < nums2[j] && i != nums1Size) {
+    if (((i != nums1Size) && (nums1[i] <= nums2[j])) || j == nums2Size) {
       nums[numsIndex] = nums1[i];
       ++i;
+      ++numsIndex;
+      continue;
     }
-    if (nums2[j] < nums1[i]) {
+    if ((j != nums2Size) && (nums2[j] <= nums1[i]) || i == nums1Size) {
       nums[numsIndex] = nums2[j];
       ++j;
+      ++numsIndex;
+      continue;
     }
-    ++numsIndex;
   }
-
-  for (int i = 0; i < numsIndex; ++i) {
-    printf("%d\n", nums[i]);
+  
+  int totalSize = nums1Size + nums2Size;
+  int remainder = totalSize % 2;
+  if (!remainder) {
+    int l_mid = (totalSize / 2) - 1;
+    int r_mid = (totalSize / 2);
+    return (nums[l_mid] + nums[r_mid] / 2);
+  } else {
+    int mid = (totalSize - 1) / 2;
+    return nums[mid];
   }
 }
 
 void main() {
-  int nums1[] = {1, 2, 3, 4, 5, 6};
-  int nums2[] = {2, 3, 4};
-  findMedianSortedArrays(nums1, 6, nums2, 3);
+  int nums1[] = {1, 3};
+  int nums2[] = {2};
+  printf("%f", findMedianSortedArrays(nums1, 2, nums2, 1));
 }
