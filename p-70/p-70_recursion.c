c233d322c329ac560b41eb09c2f49fe73490dc88
Aditya Verma <ronnaditya@gmail.com>
2026-06-01 16:42:22 +0530

    [ ERROR ] Time Complexity is insane for my solution. Trying again.

---
+++ p-70/p-70_recursion.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int climbStairs(int n) {
+  if (n <= 2) {
+    return n;
+  } else {
+    int ways = 0;
+    int i = 0;
+    while (i <= n) {
+      ways += 2
+    }
+  }
+}
+
+void main() {
+  printf("%d", climbStairs(45));
+}
