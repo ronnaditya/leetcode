94351885e6dd5c9faffa6df4b2b0c33185962057
Aditya Verma <ronnaditya@gmail.com>
2026-06-05 15:30:22 +0530

    [ PARTIAL ] Works on my system but not on LeetCode. Weird.

--- p-88/p-88.c
+++ p-88/p-88.c
@@ -5,29 +5,26 @@
 void merge(int* nums1, int nums1Size, int m, int* nums2, int nums2Size, int n) {
   int i = 0;
   int j = 0;
-  int k = 0;
+
   while (i < m + n) {
-    if (j < nums1Size) {
-      if (k < nums2Size) {
-        if (nums1[j] < nums2[k]) {
-          ++j;
-        } else {
-          for (int l = m + n - 1; l > j; --l) {
-            nums1[l] = nums1[l - 1];
-          }
-          nums1[j] = nums2[k];
-          ++k;
-          ++j;
-        }
-      } else {
-        break;
-      }
-    } else {
-      for (int l = k; l < nums2Size; ++l) {
-        nums1[i] = nums2[l];
+    if (nums1[i] == 0 & i > m) {
+      nums1[i] = nums2[j];
+      ++j;
+      ++i;
+      continue;
+    }
+    if (i < nums1Size & j < nums2Size) {
+      if (nums1[i] < nums2[j]) {
         ++i;
+      } else {
+        for (int k = m + n - 1; k > i; --k) {
+          nums1[k] = nums1[k - 1];
+        }
+        nums1[i] = nums2[j];
+        ++i;
+        ++j;
+        continue;
       }
-      break;
     }
     ++i;
   }
