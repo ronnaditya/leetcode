a546995276caf97279b02607881e1cb42e580f13
Aditya Verma <ronnaditya@gmail.com>
2026-06-05 13:14:52 +0530

    [ PARTIAL ] Successful for one case.

--- p-88/p-88.c
+++ p-88/p-88.c
@@ -3,36 +3,40 @@
 #include<string.h>
 
 void merge(int* nums1, int nums1Size, int m, int* nums2, int nums2Size, int n) {
+  int i = 0;
   int j = 0;
   int k = 0;
-  int i = 0;
-  while (i < n) {
+  while (i < m) {
     printf("i: %d | j: %d | k: %d\n", i, j, k);
-    if (nums1[j] < nums2[k]) {
-      ++j;
-    } else if (nums1[j] == nums2[k]) {
-      for (int l = nums1Size; l > j; --l) {
-        nums1[l] = nums1[l - 1];
+    if (j < nums1Size) {
+      if (k < nums2Size) {
+        if (nums1[j] < nums2[k]) {
+          ++j;
+        } else {
+          for (int l = m - 1; l > j; --l) {
+            nums1[l] = nums1[l - 1];
+          }
+          nums1[j] = nums2[k];
+          ++k;
+        }
+      } else {
+        break;
       }
-    } else if (k < nums2Size & nums1[j] > nums2[k]) {
-      for (int l = nums1Size - 1; l > j; --l) {
-        nums1[l] = nums1[l - 1];
-      }
-      nums1[j] = nums2[k];
-      ++j;
-      ++k;
     } else {
-      nums1[i] = 0;
+      for (int l = k; k < nums2Size; ++k) {
+        nums1[i] = nums2[l];
+        ++i;
+      break;
+      }
     }
     ++i;
   }
 }
 
 void main() {
-  int* arr1 = malloc(sizeof(int) * 14);
-  memcpy(arr1, (int[]){1,2,2,3,3,4,5}, sizeof(int) * 7);
+  int arr1[] = {1,1,2,3,3,3,4,5,6,0,0,0};
   int arr2[] = {1,3,3,7,7,8,10};
-  merge(arr1, 7, 12, arr2, 7, 2);
+  merge(arr1, 9, 12, arr2, 7, 3);
 
   for (int i = 0; i < 12; ++i) {
     printf("%d", arr1[i]);
