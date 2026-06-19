17ffe12b0444f0a3d25b44c9e6b2f4da7fdc5589
Aditya Verma <ronnaditya@gmail.com>
2026-06-19 09:23:49 +0530

    [ PARTIAL ] Closer

--- p-110/p-110.c
+++ p-110/p-110.c
@@ -1,10 +1,20 @@
-bool isBalanced(struct TreeNode* root) {
-    bool treeTraversal(struct TreeNode* node) {
-        if (node == NULL) {return 1;}
-        int left = treeTraversal(node->left);
-        int right = treeTraversal(node->right);
-        return abs(left - right);
-    }  
-    return treeTraversal(root);
-}
+#include<stdio.h>
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     struct TreeNode *left;
+ *     struct TreeNode *right;
+ * };
+ */
+bool isBalanced(struct TreeNode* root) { // Finally seeing with clarity.
+    int treeTraversal(int count, struct TreeNode* node) {
+        if (node == NULL) {return 0;}
+        count += 1;
+        int leftCount = treeTraversal(count, node->left);
+        int rightCount = treeTraversal(count, node->right);
 
+        return (leftCount - rightCount);
+    }
+    return treeTraversal(0, root);
+}
