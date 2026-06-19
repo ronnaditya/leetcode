9a9516efdddd30dad292d564d534ea1705c2d19a
Aditya Verma <ronnaditya@gmail.com>
2026-06-19 10:40:12 +0530

    I've run up against my limit. Time to study

--- p-110/p-110.c
+++ p-110/p-110.c
@@ -1,5 +1,3 @@
-#include<stdio.h>
-#include<stdlib.h>
 /**
  * Definition for a binary tree node.
  * struct TreeNode {
@@ -8,35 +6,6 @@
  *     struct TreeNode *right;
  * };
  */
-
-struct Count {
-    int left;
-    int right;
-};
-
 bool isBalanced(struct TreeNode* root) {
-    struct Count* treeTraversal(struct Count* count, struct TreeNode* node) {
-        if (node == NULL) {
-            return count;
-        } else if ((node->right != NULL) && (node->left == NULL)) {
-            count->right += treeTraversal(count, node->right)->right;
-            count->right += 1;
-        } else if ((node->left != NULL) && (node->right == NULL)) {
-            count->left += treeTraversal(count, node->left)->left;
-            count->left += 1;
-        } else {
-            count->left += treeTraversal(count, node->left)->left;
-            count->right += treeTraversal(count, node->right)->right;
-            count->left += 1;
-            count->right += 1;
-        }
-        return count;
-    }
-    struct Count* count = malloc(sizeof(struct Count));
-    count->left = 0;
-    count->right = 0;
-    count = treeTraversal(count, root);
-    printf("%d, %d", count->left, count->right);
-    if (abs(count->left - count->right) > 1) {return 0;}
-    else {return 1;}
+    
 }
