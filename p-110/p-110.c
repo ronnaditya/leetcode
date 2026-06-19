0f49701c113a706336d42631d0603d5203f9fb2d
Aditya Verma <ronnaditya@gmail.com>
2026-06-19 10:28:52 +0530

    [ PARTIAL ] Remains unsolved :'/

--- p-110/p-110.c
+++ p-110/p-110.c
@@ -1,4 +1,5 @@
 #include<stdio.h>
+#include<stdlib.h>
 /**
  * Definition for a binary tree node.
  * struct TreeNode {
@@ -7,14 +8,35 @@
  *     struct TreeNode *right;
  * };
  */
-bool isBalanced(struct TreeNode* root) { // Finally seeing with clarity.
-    int treeTraversal(int count, struct TreeNode* node) {
-        if (node == NULL) {return 0;}
-        count += 1;
-        int leftCount = treeTraversal(count, node->left);
-        int rightCount = treeTraversal(count, node->right);
 
-        return (leftCount - rightCount);
+struct Count {
+    int left;
+    int right;
+};
+
+bool isBalanced(struct TreeNode* root) {
+    struct Count* treeTraversal(struct Count* count, struct TreeNode* node) {
+        if (node == NULL) {
+            return count;
+        } else if ((node->right != NULL) && (node->left == NULL)) {
+            count->right += treeTraversal(count, node->right)->right;
+            count->right += 1;
+        } else if ((node->left != NULL) && (node->right == NULL)) {
+            count->left += treeTraversal(count, node->left)->left;
+            count->left += 1;
+        } else {
+            count->left += treeTraversal(count, node->left)->left;
+            count->right += treeTraversal(count, node->right)->right;
+            count->left += 1;
+            count->right += 1;
+        }
+        return count;
     }
-    return treeTraversal(0, root);
+    struct Count* count = malloc(sizeof(struct Count));
+    count->left = 0;
+    count->right = 0;
+    count = treeTraversal(count, root);
+    printf("%d, %d", count->left, count->right);
+    if (abs(count->left - count->right) > 1) {return 0;}
+    else {return 1;}
 }
