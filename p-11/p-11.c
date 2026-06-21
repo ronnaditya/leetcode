8dd5388318684f35c13221ecebd09fe588b8d359
Aditya Verma <ronnaditya@gmail.com>
2026-06-21 20:50:24 +0530

    [ RESET ]

--- p-11/p-11.c
+++ p-11/p-11.c
@@ -10,29 +10,28 @@ int minDepth(struct TreeNode* root) {
     if (root == NULL) {
         return 0;
     }
-
     int counter(struct TreeNode* node, int count) {
-        if (node != NULL) {
-            count += 1;
-            if (node->left == NULL || node->right == NULL) {
-                return count;
-            } else {
-                int leftCount = counter(node->left, count);
-                int rightCount = counter(node->right, count);
-                if (leftCount > rightCount) {
-                    return rightCount;
-                } else {
-                    return leftCount;
-                }
-            }
-        } else {
+        if (node->left == NULL && node->right == NULL) {
             return count;
+        } else {
+            int leftCount = count;
+            int rightCount = count;
+            if (node->left != NULL) {
+                leftCount = counter(node->left, leftCount + 1);
+            }
+            if (node->right != NULL) {
+                rightCount = counter(node->right, rightCount + 1);
+            }
+            printf("Val: %d, Left:%d, Right: %d\n", node->val, leftCount, rightCount);
+            if (leftCount > rightCount || leftCount == 1) {
+                return rightCount;
+            } else if (leftCount < rightCount || rightCount == 1) {
+                return leftCount;
+            } else {
+                return rightCount;
+            }
         }
     }
-
-    int count = 0;
-
-    count = counter(root, count);
-
-    return count;
+    int count = 1;
+    return counter(root, count);
 }
