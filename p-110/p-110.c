39f00467bd9faa232792946140693f23cb6c225c
Aditya Verma <ronnaditya@gmail.com>
2026-06-22 13:14:06 +0530

    [ PARTIAL ] The first intuition and 203 cases!

--- p-110/p-110.c
+++ p-110/p-110.c
@@ -7,5 +7,29 @@
  * };
  */
 bool isBalanced(struct TreeNode* root) {
-    
+    if (root == NULL) {
+        return true;
+    }
+    bool traveller(struct TreeNode* node, bool left, bool right) {
+        if (node->left != NULL && node->right != NULL) {
+            left = traveller(node->left, left, right);
+            right = traveller(node->right, left, right);
+        } else if (node->left == NULL && node->right != NULL) {
+            left = false;
+            right = traveller(node->right, left, right);
+        } else if (node->left != NULL && node->right == NULL) {
+            right = false;
+            left = traveller(node->left, left, right);
+        } else {
+            return true;
+        }
+
+        if (left && right) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    return traveller(root, true, true);
 }
